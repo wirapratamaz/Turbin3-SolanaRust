2cafacba984fb982f95ce23fa7fffad2aeb2328c
Wira Pratama Putra <89260515+wirapratamaz@users.noreply.github.com>
2025-03-27 12:21:07 +0800

    feat: empty_wallet func to transfer all remaining SOL

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ mod tests {
         signature::{Keypair, Signer, read_keypair_file}, 
         pubkey::Pubkey,
         transaction::Transaction,
-        hash::{hash}
+        hash::{hash},
+        message::Message
     };
     use solana_client::rpc_client::RpcClient;
     use solana_program::{
@@ -120,5 +121,77 @@ mod tests {
             "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
             signature
         );
+    }
+    
+    #[test]
+    fn empty_wallet() {
+        // Import our keypair
+        let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+        
+        // Define our Turbin3 public key
+        let to_pubkey = ProgramPubkey::from_str("5QpPAVrQE5aZzd9sS5pWMoXuqngGqrWUFNLcWqDCmhzT").unwrap();
+        
+        // Create a Solana devnet connection
+        let rpc_client = RpcClient::new(RPC_URL);
+        
+        // Get recent blockhash
+        let recent_blockhash = rpc_client
+            .get_latest_blockhash()
+            .expect("Failed to get recent blockhash");
+            
+        // Get balance of dev wallet
+        let balance = rpc_client
+            .get_balance(&keypair.pubkey())
+            .expect("Failed to get balance");
+            
+        println!("Current balance: {} lamports", balance);
+            
+        // Create a test transaction to calculate fees
+        let message = Message::new_with_blockhash(
+            &[transfer(
+                &keypair.pubkey(),
+                &to_pubkey,
+                balance,
+            )],
+            Some(&keypair.pubkey()),
+            &recent_blockhash
+        );
+        
+        // Calculate exact fee rate to transfer entire SOL amount out of account minus fees
+        let fee = rpc_client
+            .get_fee_for_message(&message)
+            .expect("Failed to get fee calculator");
+            
+        println!("Transaction fee: {} lamports", fee);
+        
+        // Deduct fee from lamports amount and create a TX with correct balance
+        let transaction = Transaction::new_signed_with_payer(
+            &[transfer(
+                &keypair.pubkey(),
+                &to_pubkey,
+                balance - fee,
+            )],
+            Some(&keypair.pubkey()),
+            &vec![&keypair],
+            recent_blockhash
+        );
+        
+        // Send the transaction
+        let signature = rpc_client
+            .send_and_confirm_transaction(&transaction)
+            .expect("Failed to send transaction");
+            
+        // Print our transaction out
+        println!(
+            "Success! Check out your TX here: https://explorer.solana.com/tx/{}/?cluster=devnet",
+            signature
+        );
+        
+        // Verify the new balance
+        let new_balance = rpc_client
+            .get_balance(&keypair.pubkey())
+            .expect("Failed to get balance");
+            
+        println!("New balance: {} lamports", new_balance);
     }
 }
