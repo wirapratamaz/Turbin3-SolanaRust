4e476e398a6e9d51ea32f8003d94566a38db826e
Wira Pratama Putra <89260515+wirapratamaz@users.noreply.github.com>
2025-03-27 12:09:04 +0800

    feat: implemented the keygen function

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,44 @@
 #[cfg(test)]
 mod tests {
     use solana_sdk;
+    use solana_sdk::{signature::{Keypair, Signer}, pubkey::Pubkey};
+    use bs58;
+    use std::io::{self, BufRead};
     
     #[test]
-    fn keygen() {}
+    fn keygen() {
+        // Create a new keypair
+        let kp = Keypair::new();
+        println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+        println!("");
+        println!("To save your wallet, copy and paste the following into a JSON file:");
+        println!("{:?}", kp.to_bytes());
+    }
+    
+    #[test]
+    fn base58_to_wallet() {
+        println!("Input your private key as base58:");
+        let stdin = io::stdin();
+        let base58 = stdin.lock().lines().next().unwrap().unwrap();
+        println!("Your wallet file is:");
+        let wallet = bs58::decode(base58).into_vec().unwrap();
+        println!("{:?}", wallet);
+    }
+    
+    #[test]
+    fn wallet_to_base58() {
+        println!("Input your private key as a wallet file byte array:");
+        let stdin = io::stdin();
+        let wallet = stdin.lock().lines().next().unwrap().unwrap()
+            .trim_start_matches('[')
+            .trim_end_matches(']')
+            .split(',')
+            .map(|s| s.trim().parse::<u8>().unwrap())
+            .collect::<Vec<u8>>();
+        println!("Your private key is:");
+        let base58 = bs58::encode(wallet).into_string();
+        println!("{:?}", base58);
+    }
     
     #[test]
     fn airdop() {}
